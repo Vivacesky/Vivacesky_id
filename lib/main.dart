b038aae5b53e953df03c32c24f62ca0a53276769
Furkan pinar <furkanpinar9@hotmail.com>
2020-06-07 23:16:18 +0200

    Changed to statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,142 +4,186 @@ void main() {
   runApp(MaterialApp(home: VivaceskyCard()));
 }
 
-class VivaceskyCard extends StatelessWidget {
+class VivaceskyCard extends StatefulWidget {
+  @override
+  _VivaceskyCardState createState() => _VivaceskyCardState();
+}
+
+class _VivaceskyCardState extends State<VivaceskyCard> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   final SnackBar snackBar = const SnackBar(content: Text('This is a snackbar'));
+
+  int level = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: scaffoldKey,
-        backgroundColor: Colors.grey[50],
-        appBar: AppBar(
-          title: Text('Vivacesky ID Card'),
-          centerTitle: true,
-          backgroundColor: Colors.red[900],
-          elevation: 5.0,
-          leading: Icon(Icons.menu),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add_alert),
-              tooltip: 'Open Alert',
-              onPressed: () => scaffoldKey.currentState.showSnackBar(snackBar),
-            )
-          ],
-        ),
-        body: Card(
-          elevation: 5,
-          margin: EdgeInsets.all(20),
-          child: Padding(
-            padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage: AssetImage('assets/profile.jpg'),
-                        ),
-                        title: Text('Furkan Pinar'),
-                        subtitle: Text('An application developer.'),
+      key: scaffoldKey,
+      backgroundColor: Colors.grey[50],
+      appBar: AppBar(
+        title: Text('Vivacesky ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.red[900],
+        elevation: 5.0,
+        leading: Icon(Icons.menu),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add_alert),
+            tooltip: 'Open Alert',
+            onPressed: () => scaffoldKey.currentState.showSnackBar(snackBar),
+          )
+        ],
+      ),
+      body: Card(
+        elevation: 5,
+        margin: EdgeInsets.all(20),
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage('assets/profile.jpg'),
                       ),
-                      Divider(
-                        height: 60,
-                        color: Colors.red[400],
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Icon(Icons.person),
-                          Container(
-                            margin: EdgeInsets.only(left: 10),
-                            child: Text(
-                              'NAME',
-                              style: TextStyle(
-                                  color: Colors.grey[600],
-                                  letterSpacing: 2.0,
-                                  fontSize: 14),
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Text(
-                        'Furkan Pinar',
+                      title: Text('Furkan Pinar'),
+                      subtitle: Text('An application developer.'),
+                    ),
+                    Divider(
+                      height: 60,
+                      color: Colors.red[400],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.person),
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'NAME',
+                            style: TextStyle(
+                                color: Colors.grey[600],
+                                letterSpacing: 2.0,
+                                fontSize: 14),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(
+                      'Furkan Pinar',
+                      style: TextStyle(
+                          color: Colors.red[600],
+                          letterSpacing: 1.0,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.calendar_today),
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'BIRTHDAY',
+                            style: TextStyle(
+                                color: Colors.grey[600],
+                                letterSpacing: 2.0,
+                                fontSize: 14),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(
+                      '30/06/1998',
+                      style: TextStyle(
+                          color: Colors.red[600],
+                          letterSpacing: 1.0,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.email),
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'EMAIL',
+                            style: TextStyle(
+                                color: Colors.grey[600],
+                                letterSpacing: 2.0,
+                                fontSize: 14),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text('info@furkanpinar.be',
                         style: TextStyle(
                             color: Colors.red[600],
                             letterSpacing: 1.0,
                             fontSize: 20,
-                            fontWeight: FontWeight.w700),
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Icon(Icons.calendar_today),
-                          Container(
-                            margin: EdgeInsets.only(left: 10),
-                            child: Text(
-                              'BIRTHDAY',
-                              style: TextStyle(
-                                  color: Colors.grey[600],
-                                  letterSpacing: 2.0,
-                                  fontSize: 14),
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Text(
-                        '30/06/1998',
+                            fontWeight: FontWeight.w700)),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.star),
+                        Container(
+                          margin: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'LEVEL',
+                            style: TextStyle(
+                                color: Colors.grey[600],
+                                letterSpacing: 2.0,
+                                fontSize: 14),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text('$level',
                         style: TextStyle(
                             color: Colors.red[600],
                             letterSpacing: 1.0,
                             fontSize: 20,
-                            fontWeight: FontWeight.w700),
-                      ),
-                      SizedBox(
-                        height: 15.0,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Icon(Icons.email),
-                          Container(
-                            margin: EdgeInsets.only(left: 10),
-                            child: Text(
-                              'EMAIL',
-                              style: TextStyle(
-                                  color: Colors.grey[600],
-                                  letterSpacing: 2.0,
-                                  fontSize: 14),
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Text('info@furkanpinar.be',
-                          style: TextStyle(
-                              color: Colors.red[600],
-                              letterSpacing: 1.0,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w700)),
-                    ]),
-                RaisedButton(
-                  onPressed: () {},
-                  child: Text('Logout'),
-                )
-              ],
-            ),
+                            fontWeight: FontWeight.w700)),
+                  ]),
+              RaisedButton(
+                onPressed: () {},
+                child: Text('Logout'),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red[600],
+        onPressed: () {
+          setState(() {
+            level += 1;
+          });
+        },
+      ),
+    );
   }
 }
